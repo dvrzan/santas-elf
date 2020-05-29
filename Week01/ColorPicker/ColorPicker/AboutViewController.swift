f819d5cf3d5a8cd4ae23a2ec449da7a4a1acb4f9
dvrzan <dvrzan@icloud.com>
2020-05-29 14:57:25 -0400

    Design UI

---
+++ Week01/ColorPicker/ColorPicker/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  ColorPicker
+//
+//  Created by Danijela Vrzan on 2020-05-29.
+//  Copyright © 2020 Danijela Vrzan. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+}
---
+++ Week01/ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Danijela Vrzan on 2020-05-29.
+//  Copyright © 2020 Danijela Vrzan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
