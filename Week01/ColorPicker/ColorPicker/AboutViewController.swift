612f80a16c8824f4f3f049f03a6d3bbc2328bedd
dvrzan <dvrzan@icloud.com>
2020-05-29 15:38:56 -0400

    Connect outlets and actions

--- Week01/ColorPicker/ColorPicker/AboutViewController.swift
+++ Week01/ColorPicker/ColorPicker/AboutViewController.swift
@@ -11,13 +11,27 @@ import WebKit
 
 class AboutViewController: UIViewController {
     
+    let rgbURL = "https://en.wikipedia.org/wiki/RGB_color_model"
+    
     @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadURL()
     }
     
-
+    func loadURL() {
+        if let url = URL(string: rgbURL) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+    //MARK: - Navigation
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
--- Week01/ColorPicker/ColorPicker/ViewController.swift
+++ Week01/ColorPicker/ColorPicker/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redHueSlider: UISlider!
+    @IBOutlet weak var greenSaturationSlider: UISlider!
+    @IBOutlet weak var blueBrightnessSlider: UISlider!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var colorNameLabel: UILabel!
+    @IBOutlet weak var redHueLabel: UILabel!
+    @IBOutlet weak var greenSaturationLabel: UILabel!
+    @IBOutlet weak var blueBrightnessLabel: UILabel!
+    
+    @IBOutlet weak var redHueValueLabel: UILabel!
+    @IBOutlet weak var greenSaturationValueLabel: UILabel!
+    @IBOutlet weak var blueBrightnessValueLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func setColorButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+    }
+    
 
 }
 
