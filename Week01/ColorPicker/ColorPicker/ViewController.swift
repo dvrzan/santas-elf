c43b24de8e628f3be8b5a511322e8b965ce766c1
dvrzan <dvrzan@icloud.com>
2020-05-31 07:35:27 -0400

    Implement Reset button functionality

--- Week01/ColorPicker/ColorPicker/ViewController.swift
+++ Week01/ColorPicker/ColorPicker/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var currentRedHueValue = 0
+    var currentGreenSaturationValue = 0
+    var currentBlueBrightnessValue = 0
+    
+    @IBOutlet weak var backgroundColorView: UIView!
+    
     @IBOutlet weak var redHueSlider: UISlider!
     @IBOutlet weak var greenSaturationSlider: UISlider!
     @IBOutlet weak var blueBrightnessSlider: UISlider!
@@ -24,21 +30,89 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSaturationValueLabel: UILabel!
     @IBOutlet weak var blueBrightnessValueLabel: UILabel!
     
+    @IBOutlet weak var setColorButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateSliderValueLabels()
+        
     }
-
+    
+    //MARK: - Slider Value methods
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let redHueRoundedValue = redHueSlider.value.rounded()
+        currentRedHueValue = Int(redHueRoundedValue)
+        
+        let greenSaturationRoundedValue = greenSaturationSlider.value.rounded()
+        currentGreenSaturationValue = Int(greenSaturationRoundedValue)
+        
+        let blueBrightnessRoundedValue = blueBrightnessSlider.value.rounded()
+        currentBlueBrightnessValue = Int(blueBrightnessRoundedValue)
+        
+        updateSliderValueLabels()
+    }
+    
+    func updateSliderValueLabels() {
+        redHueValueLabel.text = String(currentRedHueValue)
+        greenSaturationValueLabel.text = String(currentGreenSaturationValue)
+        blueBrightnessValueLabel.text = String(currentBlueBrightnessValue)
+    }
+    
+    //MARK: - Alert Controller methods
+    
     @IBAction func setColorButtonTapped(_ sender: UIButton) {
+        showAlert()
+    }
+    
+    func showAlert() {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Set Color", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Set", style: .default, handler: {
+            action in
+            self.getColor()
+            self.colorNameLabel.text = textField.text
+        })
+        
+        alert.addAction(action)
+        alert.addTextField { (field) in
+            textField = field
+            field.placeholder = "Enter new name for your color"
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func getColor() {
+        let redHueRoundedValue = redHueSlider.value.rounded()
+        let greenSaturationRoundedValue = greenSaturationSlider.value.rounded()
+        let blueBrightnessRoundedValue = blueBrightnessSlider.value.rounded()
+        
+        let redHue = CGFloat(redHueRoundedValue / 255) //change 255 when dealing with hue value
+        let greenSaturation = CGFloat(greenSaturationRoundedValue / 255)
+        let blueBrightness = CGFloat(blueBrightnessRoundedValue / 255)
+        
+        backgroundColorView.backgroundColor = UIColor(red: redHue, green: greenSaturation, blue: blueBrightness, alpha: 1.0)
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        backgroundColorView.backgroundColor = UIColor.systemBackground
+        
+        currentRedHueValue = 0
+        currentBlueBrightnessValue = 0
+        currentGreenSaturationValue = 0
+        
+        redHueSlider.value = Float(currentRedHueValue)
+        greenSaturationSlider.value = Float(currentGreenSaturationValue)
+        blueBrightnessSlider.value = Float(currentBlueBrightnessValue)
+        
+        updateSliderValueLabels()
     }
     
     @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        
     }
-    
 
 }
 
