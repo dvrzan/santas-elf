32ebb31961af1b40297b4fda99a7a0c5e764aeee
dvrzan <dvrzan@icloud.com>
2020-06-07 11:51:30 -0400

    Refactor to MVC

--- Week02/BullsEye/BullsEye/Controllers/ViewController.swift
+++ Week02/BullsEye/BullsEye/Controllers/ViewController.swift
@@ -9,86 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  var currentValue = 0
-  var targetValue = 0
-  var score = 0
-  var round = 0
-  
-  @IBOutlet weak var slider: UISlider!
-  @IBOutlet weak var targetLabel: UILabel!
-  @IBOutlet weak var scoreLabel: UILabel!
-  @IBOutlet weak var roundLabel: UILabel!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    let roundedValue = slider.value.rounded()
-    currentValue = Int(roundedValue)
-    startNewGame()
-  }
-
-  @IBAction func showAlert() {
     
-    let difference = abs(targetValue - currentValue)
-    var points = 100 - difference
+//    var currentValue = 0
+//    var targetValue = 0
+//    var score = 0
+//    var round = 0
     
-    score += points
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
-    let title: String
-    if difference == 0 {
-      title = "Perfect!"
-      points += 100
-    } else if difference < 5 {
-      title = "You almost had it!"
-      if difference == 1 {
-        points += 50
-      }
-    } else if difference < 10 {
-      title = "Pretty good!"
-    } else {
-      title = "Not even close..."
+    let game = BullsEyeGame(currentValue: 0, targetValue: 0, score: 0, round: 0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let roundedValue = slider.value.rounded()
+        game.currentValue = Int(roundedValue)
+        startNewGame()
     }
     
-    let message = "You scored \(points) points"
+    @IBAction func showAlert() {
+        
+//        let difference = abs(targetValue - currentValue)
+//        var points = 100 - difference
+//
+//        score += points
+//
+//        let title: String
+//        if difference == 0 {
+//            title = "Perfect!"
+//            points += 100
+//        } else if difference < 5 {
+//            title = "You almost had it!"
+//            if difference == 1 {
+//                points += 50
+//            }
+//        } else if difference < 10 {
+//            title = "Pretty good!"
+//        } else {
+//            title = "Not even close..."
+//        }
+//
+//        let message = "You scored \(points) points"
+        let title = game.calculatePoints().0
+        let message = game.calculatePoints().1
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: {
+            action in
+            self.startNewRound()
+        })
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
     
-    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        game.currentValue = Int(roundedValue)
+    }
     
-    let action = UIAlertAction(title: "OK", style: .default, handler: {
-      action in
-      self.startNewRound()
-    })
+    func startNewRound() {
+//        round += 1
+//        targetValue = Int.random(in: 1...100)
+//        currentValue = 50
+//        slider.value = Float(currentValue)
+        game.startNewRound()
+        game.currentValue = 50
+        updateViewInfo()
+    }
     
-    alert.addAction(action)
+    func updateViewInfo() {
+        targetLabel.text = String(game.targetValue)
+        scoreLabel.text = String(game.score)
+        roundLabel.text = String(game.round)
+        slider.value = Float(game.currentValue)
+    }
     
-    present(alert, animated: true, completion: nil)
+    @IBAction func startNewGame() {
+//        score = 0
+//        round = 0
+//        startNewRound()
+        game.startNewGame()
+        updateViewInfo()
+    }
     
-  }
-  
-  @IBAction func sliderMoved(_ slider: UISlider) {
-    let roundedValue = slider.value.rounded()
-    currentValue = Int(roundedValue)
-  }
-  
-  func startNewRound() {
-    round += 1
-    targetValue = Int.random(in: 1...100)
-    currentValue = 50
-    slider.value = Float(currentValue)
-    updateLabels()
-  }
-  
-  func updateLabels() {
-    targetLabel.text = String(targetValue)
-    scoreLabel.text = String(score)
-    roundLabel.text = String(round)
-  }
-  
-  @IBAction func startNewGame() {
-    score = 0
-    round = 0
-    startNewRound()
-  }
-  
 }
 
 
--- Week02/BullsEye/BullsEye/Models/BullsEyeGame.swift
+++ Week02/BullsEye/BullsEye/Models/BullsEyeGame.swift
@@ -8,8 +8,55 @@
 
 import Foundation
 
-struct BullsEyeGame {
+class BullsEyeGame {
     
+    var currentValue: Int
+    var targetValue: Int
+    var score: Int
+    var round: Int
     
+    init(currentValue: Int, targetValue: Int, score: Int, round: Int) {
+        self.currentValue = currentValue
+        self.targetValue = targetValue
+        self.score = score
+        self.round = round
+    }
+    
+    func calculatePoints() -> (String, String) {
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        
+        score += points
+
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        let message = "You scored \(points) points"
+
+        return (title, message)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+    }
     
 }
