2a266159a5380471f0fd0b1288f30a5df0646d10
dvrzan <dvrzan@icloud.com>
2020-06-07 09:56:43 -0400

    Add constraints

---
+++ Week02/BullsEye/BullsEye/Models/BullsEyeGame.swift
@@ -0,0 +1,15 @@
+//
+//  BullsEyeGame.swift
+//  BullsEye
+//
+//  Created by Danijela Vrzan on 2020-06-07.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+struct BullsEyeGame {
+    
+    
+    
+}
